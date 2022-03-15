381377e7db5d643bd200f6a0c3dbb29671eaefe1
Batteredburrito <Cameron_willer1995@hotmail.com>
2022-03-15 10:09:18 +0000

    Uninstallations Script for MCPR tool

---
+++ README.txt
@@ -0,0 +1,9 @@
+package this app as an IntuneWin file. Specify the install command as:
+
+powershell.exe -executionpolicy bypass -command "& '.\install.ps1'"
+
+Detection rule is as follows:
+
+Registry HKEY_LOCAL_MACHINE\SOFTWARE\McAfee 
+
+Key Does Not Exist
---
+++ install.ps1
@@ -0,0 +1,10 @@
+﻿# Run the cleanup tool
+$program= ".\McCleanup.exe"
+$programArg= "-p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s"
+$process = Start-Process $program -ArgumentList $ProgramArg -passthru -Wait -NoNewWindow
+
+# Remove the Store apps from McAfee
+$RemoveApp = 'Mcafee'
+Get-AppxPackage -AllUsers | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage
+Get-AppxPackage | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -Match $RemoveApp} | Remove-AppxProvisionedPackage -Online
