5b75d6ca74c672384507bcb21856c8aabc0190ad
Batteredburrito <Cameron_willer1995@hotmail.com>
2022-03-15 10:06:37 +0000

    Lenovo IM Controller uninstall script

---
+++ LenovoIMController/install.ps1
@@ -0,0 +1,46 @@
+﻿#App Name
+$directoryApp = "C:\Windows\System32\IMController.InfInstaller.exe"
+#Dependency Name
+$depName = "C:\Windows\System32\IMController.CoInstaller.dll"
+#Directory Path
+$directory = "C:\Windows\System32"
+
+#Variable for IMController Existin
+$existApp = Test-Path $directoryApp -ErrorAction SilentlyContinue
+#Variable for Confirming Uninstall based on dependency not existing
+$unSuccess = Test-Path $depName -ErrorAction SilentlyContinue
+$cleanSuccess = Test-Path $directoryApp -ErrorAction SilentlyContinue
+
+
+#If Process is running, stop process, else, state its not running
+if($existApp){
+    #Call IMController Uninstall and wait
+    Start-Process -NoNewWindow -FilePath $directoryApp -ArgumentList "-uninstall" -Wait
+}
+else {
+    Write-Output "IMController doesnt exist"
+}
+
+#Test if DLL Still exists, if so, delete it and IM Controller. If not, skip
+if ($unSuccess){
+    Write-Output "IM DLL exists"
+    Remove-Item -Path "C:\Windows\System32\IMController.InfInstaller.exe" -Force
+}
+else {
+    Write-Host "IM DLL doesnt exist"
+    #Remove IMController if it exists
+    if($existApp){
+    Remove-Item -Path "C:\Windows\System32\IMController.InfInstaller.exe" -Force
+    }
+    else {
+    Write-Output "All Clean"
+    }
+}
+
+#Test Uninstall Success
+if($cleanSuccess){
+    Write-Output "Cleanup Failed"
+    }
+    else{
+    Write-Output "Cleanup Succeeded"
+    }
